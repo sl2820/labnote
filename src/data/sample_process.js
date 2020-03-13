d5e80ab022a030803cf200372dc1e7d2ac0831b8
Sukjoo Hong <hngskj@gmail.com>
2020-03-13 17:14:43 +0900

    Add new sample process db

---
+++ src/data/sample_process.js
@@ -0,0 +1,53 @@
+export default {
+  name: "process",
+  functions: [
+    {
+      name: "Mix"
+    },
+    {
+      name: "Inject"
+    },
+    {
+      name: "Wait"
+    },
+    {
+      name: "Mix"
+    },
+    {
+      name: "Add"
+    },
+    {
+      name: "Washing"
+    },
+    {
+      name: "Incubating"
+    },
+    {
+      name: "Centrifuge"
+    },
+    {
+      name: "Stirring"
+    },
+    {
+      name: "Disperse"
+    },
+    {
+      name: "Sonicate"
+    },
+    {
+      name: "Furnace"
+    },
+    {
+      name: "Cooling"
+    },
+    {
+      name: "Annealing"
+    },
+    {
+      name: "Dissolve"
+    },
+    {
+      name: "Heat"
+    }
+  ]
+}
