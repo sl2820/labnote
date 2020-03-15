4fa09dac70854a06926f56946ce1cd10c039f625
jngynchoi <61965637+jngynchoi@users.noreply.github.com>
2020-03-16 03:54:08 +0900

    Update sample_process.js

--- src/data/sample_process.js
+++ src/data/sample_process.js
@@ -2,52 +2,164 @@ export default {
   name: "process",
   functions: [
     {
-      name: "Mix"
+          name: "Mix",
+          inputs: [
+              {
+                  id: null,
+                  amount: null
+              },
+              {
+                  id: null,
+                  amount: null
+              }
+          ],
+          gradually: false,
+          time:0
     },
     {
-      name: "Inject"
+        name: "Inject",
+        chem_for_inject: {
+            id: null,
+            amount:null
+        },
+        to: {
+            id: null,
+            amount:null
+        },
+        stirring: true,
+        gradually: false,
+        time:0
     },
     {
-      name: "Wait"
+        name: "Wait",
+        for: "Growth",
+        time:0
     },
     {
-      name: "Mix"
+        name: "Add",
+        chem_for_add: {
+            id: null,
+            amount:null
+        },
+        to: {
+            id: null,
+            amount:null
+        },
+        gradually: false,
+        time:0
     },
     {
-      name: "Add"
+        name: "Washing",
+        chem_for_wash: {
+            id:null
+        },
+        washer: null,
+        times: 1,
+        rpm: null,
+        time:0
     },
     {
-      name: "Washing"
+        name: "Incubating",
+        chem_for_incubate: {
+            id:null
+        },
+        temperature: null,
+        time:0
     },
     {
-      name: "Incubating"
+        name: "Centrifuge",
+        chem_for_centrifuge: {
+            id:null
+        },
+        rpm: null,
+        time:0
     },
     {
-      name: "Centrifuge"
+        name: "Stirring",
+        chem_for_stir: {
+            id:null
+        },
+        rpm: null,
+        time:0
     },
     {
-      name: "Stirring"
+        name: "Dry",
+        chem_for_dry: {
+            id:null
+        },
+        temperature: null,
+        time: 0,
+        vacuum:false
     },
     {
-      name: "Disperse"
+        name: "Disperse",
+        chem_for_disperse: {
+            id: null,
+            amount:null
+        },
+        disperser: {
+            name: null,
+            amount:null
+        },
+        gradually: false,
+        time:0
     },
     {
-      name: "Sonicate"
+        name: "Sonicate",
+        chem_for_sonicate: {
+            id:null
+        },
+        temperature: null,
+        time:0
     },
     {
-      name: "Furnace"
+        name: "Furnace",
+        chem_for_heat: {
+            id:null
+        },
+        temperature: null,
+        ramping_rate: null,
+        time: 0,
+        feeding_gas:null
     },
     {
-      name: "Cooling"
+        name: "Cooling",
+        chem_for_cool: {
+            id:null
+        },
+        temperature: null,
+        time: 0,
+        feeding_gas:null
     },
     {
-      name: "Annealing"
+        name: "Annealing",
+        chem_for_anneal: {
+            id:null
+        },
+        temperature: null,
+        time: 0,
+        atmosphere:null
     },
     {
-      name: "Dissolve"
+        name: "Dissolve",
+        solute: {
+            id: null,
+            amount:null
+        },
+        solvent: {
+            name: null,
+            amount:null
+        },
+        gradually: false,
+        time:0
     },
     {
-      name: "Heat"
+        name: "Heat",
+        chem_for_heat: {
+            id:null
+        },
+        temperature: null,
+        time:0
     }
   ]
 }
