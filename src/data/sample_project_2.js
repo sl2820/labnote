d766a88bc5db4400f90244770b1402202f336ff0
Hyunchul Kim <hynchl.kim@gmail.com>
2020-03-31 21:03:36 +0900

    added warning! `id` and `used_id` should be INTEGER, and we will use `20` as user_id before implementing more user functions.

--- src/data/sample_project_2.js
+++ src/data/sample_project_2.js
@@ -1,6 +1,7 @@
 // data
 // import { uuid } from "./utils"
-
+/* ID should be integer!!! */
+/* user_id should be 20!!! */
 export default {
   id: "2",
   user_id: 20,
--- src/data/sample_project_empty.js
+++ src/data/sample_project_empty.js
@@ -1,7 +1,11 @@
 // data
 // import { uuid } from "./utils"
 
+/* ID should be integer!!! */
+/* user_id should be 20!!! */
+
 export default {
-  id: "empty1234",
+  id: "5",
+  user_id: 20,
   tasks: []
 }
